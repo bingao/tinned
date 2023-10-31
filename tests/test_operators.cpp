7cf09eb695ffaaab8d87eee7f78baa6a721e28e4
Bin Gao <bin.gao@uit.no>
2023-10-31 12:44:56 +0100

    add Catch2 for unit testing

--- tests/test_operators.cpp
+++ tests/test_operators.cpp
@@ -1,163 +1,219 @@
-#include <iostream>
+#define CATCH_CONFIG_MAIN
+
+#include <cstddef>
+#include <set>
 #include <string>
-#include <utility>
 
-#include <symengine/dict.h>
-#include <symengine/constants.h>
+#include <catch2/catch.hpp>
+
+#include <symengine/basic.h>
+#include <symengine/integer.h>
+#include <symengine/rational.h>
+#include <symengine/real_double.h>
 #include <symengine/symengine_rcp.h>
-#include <symengine/matrices/matrix_add.h>
-#include <symengine/matrices/matrix_mul.h>
-
-#include <symengine/subs.h>
-
-#include <symengine/functions.h>
-#include <symengine/mul.h>
 
 #include "Tinned.hpp"
 
-int main()
+TEST_CASE("Perturbation", "[operators]")
 {
-    auto f = SymEngine::make_rcp<const Tinned::Perturbation>(
+    auto el0 = SymEngine::make_rcp<const Tinned::Perturbation>(
         std::string("EL"),
-        SymEngine::one
+        SymEngine::integer(1)
     );
-    auto g = SymEngine::make_rcp<const Tinned::Perturbation>(
-        std::string("GEO"),
-        SymEngine::two
+    auto el1 = SymEngine::make_rcp<const Tinned::Perturbation>(
+        std::string("EL"),
+        SymEngine::rational(2, 5)
     );
-    std::cout << Tinned::stringify(*f) << "\n";
-    std::cout << Tinned::stringify(*g) << "\n\n";
-
-    auto S = SymEngine::make_rcp<const Tinned::OneElecOperator>(
-        std::string("S"),
-        Tinned::PertDependency({
-            //std::make_pair(f, 0),  //can be removed
-            std::make_pair(g, 99)
-        })
+    auto el2 = SymEngine::make_rcp<const Tinned::Perturbation>(
+        std::string("EL"),
+        SymEngine::real_double(1.5)
     );
-    std::cout << Tinned::stringify(*S) << "\n\n";
+    REQUIRE(SymEngine::neq(*el0, *el1));
+    REQUIRE(SymEngine::neq(*el0, *el2));
+    REQUIRE(SymEngine::neq(*el1, *el2));
 
-    auto D = SymEngine::make_rcp<const Tinned::OneElecDensity>(std::string("D"));
-    std::cout << Tinned::stringify(*D) << "\n\n";
-
-    // idempotency constraint Z = DSD - D
-    auto Z = SymEngine::matrix_add({
-        SymEngine::matrix_mul({D, S, D}),
-        SymEngine::matrix_mul({SymEngine::minus_one, D})
-    });
-    std::cout << Tinned::stringify(*Z) << "\n\n";
-
-    auto K = (Z->diff(g))->diff(g);
-    std::cout << Tinned::stringify(*K) << "\n\n";
-
-    auto Dg = D->diff(g);
-    auto Dgg = Dg->diff(g);
-    auto Keq = Tinned::remove_if(K, SymEngine::vec_basic({Dg}));
-    std::cout << "remove_if(): " << Tinned::stringify(*Keq) << "\n\n";
-    Keq = Tinned::remove_if(K, SymEngine::vec_basic({Dgg}));
-    std::cout << "remove_if(): " << Tinned::stringify(*Keq) << "\n\n";
-    Keq = Tinned::remove_if(K, SymEngine::vec_basic({Dg, Dgg}));
-    std::cout << "remove_if(): " << Tinned::stringify(*Keq) << "\n\n";
-
-    auto Kneq = Tinned::keep_if(K, SymEngine::vec_basic({Dg}));
-    std::cout << "keep_if(): " << Tinned::stringify(*Kneq) << "\n\n";
-    Kneq = Tinned::keep_if(K, SymEngine::vec_basic({Dgg}));
-    std::cout << "keep_if(): " << Tinned::stringify(*Kneq) << "\n\n";
-    Kneq = Tinned::keep_if(K, SymEngine::vec_basic({Dg, Dgg}));
-    std::cout << "keep_if(): " << Tinned::stringify(*Kneq) << "\n\n";
-
-    // replacement
-    //auto Dp = SymEngine::make_rcp<const Tinned::OneElecDensity>(std::string("Dp"));
-    //SymEngine::map_basic_basic Msub;
-    //Msub[D] = Dp;
-    //auto M = Tinned::replace(K, Msub, true);
-    //std::cout << "M = " << Tinned::stringify(*M) << "\n\n";
-
-    auto r = SymEngine::function_symbol("r", SymEngine::vec_basic({f, g}));
-    //auto s = SymEngine::function_symbol("s", SymEngine::vec_basic({f, g}));
-    auto s = SymEngine::function_symbol("s", SymEngine::vec_basic({r}));
-    std::cout << "s: " << Tinned::stringify(s) << "\n";
-    std::cout << "ds/df: " << Tinned::stringify(s->diff(f)) << "\n";
-    std::cout << "d2s/df2: " << Tinned::stringify((s->diff(f))->diff(f)) << "\n";
-    return 0;
-
-    auto prod = SymEngine::mul({r, s});
-    //auto deriv = (prod->diff(f))->diff(g);
-    auto deriv = prod->diff(f);
-    SymEngine::map_basic_basic psub;
-    psub[r] = f;
-    psub[g] = f;
-    //psub[(r->diff(f))->diff(f)] = f;
-    std::cout << "prod = " << Tinned::stringify(*prod) << "\n";
-    std::cout << "deriv = " << Tinned::stringify(*deriv) << "\n";
-    std::cout << "msubs = " << Tinned::stringify(*SymEngine::msubs(deriv, psub)) << "\n";
-    std::cout << "xreplace = " << Tinned::stringify(*deriv->xreplace(psub)) << "\n\n";
-
-    return 0;
-
-    auto G = SymEngine::make_rcp<const Tinned::TwoElecOperator>(
-        std::string("G"),
-        D,
-        Tinned::PertDependency({std::make_pair(g, 99)})
-    );
-    std::cout << Tinned::stringify(*G) << "\n\n";
-    std::cout << Tinned::stringify(*(((G->diff(g))->diff(f))->diff(g))) << "\n\n";
-
-    auto Exc = SymEngine::make_rcp<const Tinned::ExchCorrEnergy>(
-        std::string("Exc"), D
-    );
-    std::cout << Tinned::stringify(*Exc) << "\n\n";
-    std::cout << Tinned::stringify(*(((Exc->diff(g))->diff(f))->diff(g))) << "\n\n";
-
-    auto Fxc = SymEngine::make_rcp<const Tinned::ExchCorrPotential>(
-        std::string("Fxc"), D
-    );
-    std::cout << Tinned::stringify(*Fxc) << "\n\n";
-    std::cout << Tinned::stringify(*(((Fxc->diff(g))->diff(f))->diff(g))) << "\n\n";
-
-    auto h_nuc = SymEngine::make_rcp<const Tinned::NonElecFunction>(
-        std::string("h_nuc"),
-        Tinned::PertDependency({
-            std::make_pair(f, 99),
-            std::make_pair(g, 99)
-        })
-    );
-    std::cout << Tinned::stringify(*h_nuc) << "\n\n";
-    std::cout << Tinned::stringify(*(((h_nuc->diff(g))->diff(f))->diff(g))) << "\n\n";
-
-    auto Dt = SymEngine::make_rcp<const Tinned::TemporumOperator>(D);
-    auto Dw = SymEngine::rcp_dynamic_cast<const Tinned::TemporumOperator>((Dt->diff(g))->diff(g));
-    std::cout << Tinned::stringify(*Dt) << "\n\n";
-    std::cout << Tinned::stringify(*Dw) << "\n";
-    std::cout << "frequnecy: " << Tinned::stringify(*Dw->get_frequency()) << "\n\n";
-
-    auto T = SymEngine::make_rcp<const Tinned::TemporumOverlap>(
-        Tinned::PertDependency({std::make_pair(g, 99), std::make_pair(f, 99)})
-    );
-    std::cout << Tinned::stringify(*T) << "\n\n";
-    auto Tg = T->diff(g);
-    std::cout << Tinned::stringify(*Tg) << "\n\n";
-
-    auto Tgg = SymEngine::rcp_dynamic_cast<const Tinned::TemporumOverlap>(Tg->diff(g));
-    std::cout << Tinned::stringify(*Tgg) << "\n";
-    std::cout << "size: " << Tgg->size() << "\n";
-    for (std::size_t i = 0; i<Tgg->size(); ++i) {
-        std::cout << "frequnecy: " << Tinned::stringify(*Tgg->get_frequency(i));
-        auto braket = Tgg->get_braket_product(i);
-        std::cout << ", coef.: " << Tinned::stringify(*std::get<0>(braket))
-                  << ", bra: " << Tinned::stringify(*std::get<1>(braket))
-                  << ", ket: " << Tinned::stringify(*std::get<2>(braket))
-                  << "\n\n";
-    }
-    auto braket1 = Tgg->get_braket_product(1);
-    auto braket2 = Tgg->get_braket_product(2);
-    std::cout << "equal: " << std::get<0>(braket1)->__eq__(*std::get<0>(braket2)) << "\n";
-    std::cout << "equal: " << std::get<1>(braket1)->__eq__(*std::get<1>(braket2)) << "\n";
-    std::cout << "bra: " << Tinned::stringify(*std::get<1>(braket1)) << "\n"
-              << "bra: " << Tinned::stringify(*std::get<1>(braket2)) << "\n";
-    std::cout << "equal: " << std::get<2>(braket1)->__eq__(*std::get<2>(braket2)) << "\n";
-    std::cout << "ket: " << Tinned::stringify(*std::get<2>(braket1)) << "\n"
-              << "ket: " << Tinned::stringify(*std::get<2>(braket2)) << "\n";
-
-    return 0;
+    //auto geo = SymEngine::make_rcp<const Tinned::Perturbation>(
+    //    std::string("GEO"),
+    //    SymEngine::zero
+    //);
+    //REQUIRE();
 }
+
+TEST_CASE("ElectronicState", "[operators]")
+{
+}
+
+TEST_CASE("OneElecOperator", "[operators]")
+{
+}
+
+TEST_CASE("TwoElecOperator", "[operators]")
+{
+}
+
+TEST_CASE("ExchCorrEnergy", "[operators]")
+{
+}
+
+TEST_CASE("ExchCorrPotential", "[operators]")
+{
+}
+
+TEST_CASE("NonElecFunction", "[operators]")
+{
+}
+
+TEST_CASE("TemporumOperator", "[operators]")
+{
+}
+
+TEST_CASE("TemporumOverlap", "[operators]")
+{
+}
+
+//int main()
+//{
+//    auto f = SymEngine::make_rcp<const Tinned::Perturbation>(
+//        std::string("EL"),
+//        SymEngine::one
+//    );
+//    auto g = SymEngine::make_rcp<const Tinned::Perturbation>(
+//        std::string("GEO"),
+//        SymEngine::two
+//    );
+//    std::cout << Tinned::stringify(*f) << "\n";
+//    std::cout << Tinned::stringify(*g) << "\n\n";
+//
+//    auto S = SymEngine::make_rcp<const Tinned::OneElecOperator>(
+//        std::string("S"),
+//        Tinned::PertDependency({
+//            //std::make_pair(f, 0),  //can be removed
+//            std::make_pair(g, 99)
+//        })
+//    );
+//    std::cout << Tinned::stringify(*S) << "\n\n";
+//
+//    auto D = SymEngine::make_rcp<const Tinned::OneElecDensity>(std::string("D"));
+//    std::cout << Tinned::stringify(*D) << "\n\n";
+//
+//    // idempotency constraint Z = DSD - D
+//    auto Z = SymEngine::matrix_add({
+//        SymEngine::matrix_mul({D, S, D}),
+//        SymEngine::matrix_mul({SymEngine::minus_one, D})
+//    });
+//    std::cout << Tinned::stringify(*Z) << "\n\n";
+//
+//    auto K = (Z->diff(g))->diff(g);
+//    std::cout << Tinned::stringify(*K) << "\n\n";
+//
+//    auto Dg = D->diff(g);
+//    auto Dgg = Dg->diff(g);
+//    auto Keq = Tinned::remove_if(K, SymEngine::vec_basic({Dg}));
+//    std::cout << "remove_if(): " << Tinned::stringify(*Keq) << "\n\n";
+//    Keq = Tinned::remove_if(K, SymEngine::vec_basic({Dgg}));
+//    std::cout << "remove_if(): " << Tinned::stringify(*Keq) << "\n\n";
+//    Keq = Tinned::remove_if(K, SymEngine::vec_basic({Dg, Dgg}));
+//    std::cout << "remove_if(): " << Tinned::stringify(*Keq) << "\n\n";
+//
+//    auto Kneq = Tinned::keep_if(K, SymEngine::vec_basic({Dg}));
+//    std::cout << "keep_if(): " << Tinned::stringify(*Kneq) << "\n\n";
+//    Kneq = Tinned::keep_if(K, SymEngine::vec_basic({Dgg}));
+//    std::cout << "keep_if(): " << Tinned::stringify(*Kneq) << "\n\n";
+//    Kneq = Tinned::keep_if(K, SymEngine::vec_basic({Dg, Dgg}));
+//    std::cout << "keep_if(): " << Tinned::stringify(*Kneq) << "\n\n";
+//
+//    // replacement
+//    //auto Dp = SymEngine::make_rcp<const Tinned::OneElecDensity>(std::string("Dp"));
+//    //SymEngine::map_basic_basic Msub;
+//    //Msub[D] = Dp;
+//    //auto M = Tinned::replace(K, Msub, true);
+//    //std::cout << "M = " << Tinned::stringify(*M) << "\n\n";
+//
+//    auto r = SymEngine::function_symbol("r", SymEngine::vec_basic({f, g}));
+//    //auto s = SymEngine::function_symbol("s", SymEngine::vec_basic({f, g}));
+//    auto s = SymEngine::function_symbol("s", SymEngine::vec_basic({r}));
+//    std::cout << "s: " << Tinned::stringify(s) << "\n";
+//    std::cout << "ds/df: " << Tinned::stringify(s->diff(f)) << "\n";
+//    std::cout << "d2s/df2: " << Tinned::stringify((s->diff(f))->diff(f)) << "\n";
+//    return 0;
+//
+//    auto prod = SymEngine::mul({r, s});
+//    //auto deriv = (prod->diff(f))->diff(g);
+//    auto deriv = prod->diff(f);
+//    SymEngine::map_basic_basic psub;
+//    psub[r] = f;
+//    psub[g] = f;
+//    //psub[(r->diff(f))->diff(f)] = f;
+//    std::cout << "prod = " << Tinned::stringify(*prod) << "\n";
+//    std::cout << "deriv = " << Tinned::stringify(*deriv) << "\n";
+//    std::cout << "msubs = " << Tinned::stringify(*SymEngine::msubs(deriv, psub)) << "\n";
+//    std::cout << "xreplace = " << Tinned::stringify(*deriv->xreplace(psub)) << "\n\n";
+//
+//    return 0;
+//
+//    auto G = SymEngine::make_rcp<const Tinned::TwoElecOperator>(
+//        std::string("G"),
+//        D,
+//        Tinned::PertDependency({std::make_pair(g, 99)})
+//    );
+//    std::cout << Tinned::stringify(*G) << "\n\n";
+//    std::cout << Tinned::stringify(*(((G->diff(g))->diff(f))->diff(g))) << "\n\n";
+//
+//    auto Exc = SymEngine::make_rcp<const Tinned::ExchCorrEnergy>(
+//        std::string("Exc"), D
+//    );
+//    std::cout << Tinned::stringify(*Exc) << "\n\n";
+//    std::cout << Tinned::stringify(*(((Exc->diff(g))->diff(f))->diff(g))) << "\n\n";
+//
+//    auto Fxc = SymEngine::make_rcp<const Tinned::ExchCorrPotential>(
+//        std::string("Fxc"), D
+//    );
+//    std::cout << Tinned::stringify(*Fxc) << "\n\n";
+//    std::cout << Tinned::stringify(*(((Fxc->diff(g))->diff(f))->diff(g))) << "\n\n";
+//
+//    auto h_nuc = SymEngine::make_rcp<const Tinned::NonElecFunction>(
+//        std::string("h_nuc"),
+//        Tinned::PertDependency({
+//            std::make_pair(f, 99),
+//            std::make_pair(g, 99)
+//        })
+//    );
+//    std::cout << Tinned::stringify(*h_nuc) << "\n\n";
+//    std::cout << Tinned::stringify(*(((h_nuc->diff(g))->diff(f))->diff(g))) << "\n\n";
+//
+//    auto Dt = SymEngine::make_rcp<const Tinned::TemporumOperator>(D);
+//    auto Dw = SymEngine::rcp_dynamic_cast<const Tinned::TemporumOperator>((Dt->diff(g))->diff(g));
+//    std::cout << Tinned::stringify(*Dt) << "\n\n";
+//    std::cout << Tinned::stringify(*Dw) << "\n";
+//    std::cout << "frequnecy: " << Tinned::stringify(*Dw->get_frequency()) << "\n\n";
+//
+//    auto T = SymEngine::make_rcp<const Tinned::TemporumOverlap>(
+//        Tinned::PertDependency({std::make_pair(g, 99), std::make_pair(f, 99)})
+//    );
+//    std::cout << Tinned::stringify(*T) << "\n\n";
+//    auto Tg = T->diff(g);
+//    std::cout << Tinned::stringify(*Tg) << "\n\n";
+//
+//    auto Tgg = SymEngine::rcp_dynamic_cast<const Tinned::TemporumOverlap>(Tg->diff(g));
+//    std::cout << Tinned::stringify(*Tgg) << "\n";
+//    std::cout << "size: " << Tgg->size() << "\n";
+//    for (std::size_t i = 0; i<Tgg->size(); ++i) {
+//        std::cout << "frequnecy: " << Tinned::stringify(*Tgg->get_frequency(i));
+//        auto braket = Tgg->get_braket_product(i);
+//        std::cout << ", coef.: " << Tinned::stringify(*std::get<0>(braket))
+//                  << ", bra: " << Tinned::stringify(*std::get<1>(braket))
+//                  << ", ket: " << Tinned::stringify(*std::get<2>(braket))
+//                  << "\n\n";
+//    }
+//    auto braket1 = Tgg->get_braket_product(1);
+//    auto braket2 = Tgg->get_braket_product(2);
+//    std::cout << "equal: " << std::get<0>(braket1)->__eq__(*std::get<0>(braket2)) << "\n";
+//    std::cout << "equal: " << std::get<1>(braket1)->__eq__(*std::get<1>(braket2)) << "\n";
+//    std::cout << "bra: " << Tinned::stringify(*std::get<1>(braket1)) << "\n"
+//              << "bra: " << Tinned::stringify(*std::get<1>(braket2)) << "\n";
+//    std::cout << "equal: " << std::get<2>(braket1)->__eq__(*std::get<2>(braket2)) << "\n";
+//    std::cout << "ket: " << Tinned::stringify(*std::get<2>(braket1)) << "\n"
+//              << "ket: " << Tinned::stringify(*std::get<2>(braket2)) << "\n";
+//
+//    return 0;
+//}
