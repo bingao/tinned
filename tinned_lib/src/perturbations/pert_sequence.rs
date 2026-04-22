567587a0f7ad7fd79b70a0ffdc74c07a21e6b8c9
Bin Gao <bin.gao@uit.no>
2026-04-22 11:09:51 +0200

    add Display for dyn PertSequence

--- tinned_lib/src/perturbations/pert_sequence.rs
+++ tinned_lib/src/perturbations/pert_sequence.rs
@@ -1,4 +1,5 @@
 use std::collections::BTreeMap;
+use std::fmt;
 use std::sync::Arc;
 
 use crate::perturbations::{PertMultichain, Perturbation};
@@ -6,6 +7,16 @@ use crate::perturbations::{PertMultichain, Perturbation};
 pub trait PertSequence {
     fn as_vec(&self) -> Vec<Arc<Perturbation>>;
     fn as_map(&self) -> BTreeMap<Arc<Perturbation>, u32>;
+
+    fn fmt_sequence(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        for (i, p) in self.as_vec().iter().enumerate() {
+            if i > 0 {
+                f.write_str(";")?;
+            }
+            write!(f, "{p}")?;
+        }
+        Ok(())
+    }
 }
 
 impl PertSequence for &[Arc<Perturbation>] {
@@ -51,5 +62,11 @@ impl PertSequence for PertMultichain {
     #[inline]
     fn as_map(&self) -> BTreeMap<Arc<Perturbation>, u32> {
         self.get_map_clone()
+    }
+}
+
+impl fmt::Display for dyn PertSequence + '_ {
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        self.fmt_sequence(f)
     }
 }
